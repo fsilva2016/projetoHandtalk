909e033a3ed4bea0432bed6c8e41c5dec8400fff
Felipe Moraes <felipedasilva1990@yahoo.com.br>
2022-03-05 23:14:05 -0300

    Intalação do Antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import { useEffect, useState } from 'react';
 
 function App() {
+  useEffect(() => {},[])
+  const [ showNav,setshowNav] = useState(false)
+
+  const overNavFunction = () =>{
+    setshowNav(prev => !prev)
+    console.log(showNav)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">     
+        
+        <nav className="navPlug">
+          <li><a href="#">Plung</a></li>
+          <li><a href="#">Aplicativo</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contudrteerto</a></li>
+        </nav>
       </header>
-    </div>
+      <div>
+    <Button type="primary">
+      olas
+    </Button>
+      </div>
+    </div>   
   );
 }
 
